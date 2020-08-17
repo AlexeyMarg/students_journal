a4c8443ff04c56fcad070975985b195350b5710d
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2020-08-17 21:15:49 +0300

    Prototypes af add and remove functions, dynamic table

--- application_class.py
+++ application_class.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import os
 import sqlite3
 import tkinter.ttk as ttk
+import tkinter.messagebox as mb
 
 
 class Application:
@@ -12,7 +13,7 @@ class Application:
         self.subject_list = self.get_subjects()
         self.subject_var = tk.StringVar()
         self.subject_var.set(self.subject_list[0])
-        self.subject_option = tk.OptionMenu(parent, self.subject_var, *self.subject_list)
+        self.subject_option = tk.OptionMenu(parent, self.subject_var, *self.subject_list, command=self.subject_selection)
         self.subject_option.config(width=15)
         self.subject_option.grid(row=1, column=1, padx=2, pady=2)
         self.subject_add_button = tk.Button(parent, text='Add subject', command=self.press_add_subject)
@@ -26,10 +27,9 @@ class Application:
 
         self.subject_headers = self.get_headers(self.subject_var.get())
         self.subject_rows = self.get_rows(self.subject_var.get())
-        print(self.subject_rows)
 
-        table = Table(parent, headings=self.subject_headers, rows=self.subject_rows)
-        table.grid(row=2, column=1, columnspan=5)
+        self.table = Table(parent, headings=self.subject_headers, rows=self.subject_rows)
+        self.table.grid(row=2, column=1, columnspan=len(self.subject_headers))
 
 
     def connect_db(self):
@@ -50,6 +50,15 @@ class Application:
         res = self.cursor.fetchall()
         return res
 
+    def subject_selection(self, event):
+        self.table.destroy()
+        table = self.subject_var.get()
+        headers = self.get_headers(table)
+        rows = self.get_rows(table)
+        self.table = Table(self.parent, headings=headers, rows=rows)
+        self.table.grid(row=2, column=1, columnspan=len(headers))
+
+
     def _delete_window(self):
         self.conn.commit()
         self.conn.close()
@@ -63,16 +72,51 @@ class Application:
         return subject_list
 
     def press_add_subject(self):
-        pass
+        self.add_subject_window = tk.Toplevel(self.parent)
+        self.add_subject_window.title('Add subject')
+        self.add_subject_label = tk.Label(self.add_subject_window, text='Enter headings via ,:')
+        self.add_subject_label.grid(row=1, column=1, padx=2, pady=2, sticky='w')
+        self.add_subject_entry = tk.Entry(self.add_subject_window, width=50)
+        self.add_subject_entry.grid(row=2, column=1, padx=2, pady=2)
+        self.add_subject_button = tk.Button( self.add_subject_window, text='Add', width=10, command=self.add_subject)
+        self.add_subject_button.grid(row=3, column=1, padx=2, pady=2)
+
+    def add_subject(self):
+        print(self.add_subject_entry.get())
 
     def press_del_subject(self):
-        pass
+        answer = mb.askyesno(title="Removing", message="Do you want to remove selected subject?")
+        if answer:
+            print('Subject is removed')
+
 
     def press_add_student(self):
-        pass
+        self.add_student_window = tk.Toplevel(self.parent)
+        self.add_student_window.title('Add student')
+        self.add_student_label1 = tk.Label(self.add_student_window, text='Enter student data via , for headers:')
+        self.add_student_label1.grid(row=1, column=1, padx=2, pady=2, sticky='w')
+        s = ''
+        for i in self.subject_headers:
+            s=s+i+', '
+        s = s[:-2]
+        self.add_student_label2 = tk.Label(self.add_student_window, text=s)
+        self.add_student_label2.grid(row=2, column=1, padx=2, pady=2, sticky='w')
+        self.add_student_entry = tk.Entry(self.add_student_window, width=50)
+        self.add_student_entry.grid(row=3, column=1, padx=2, pady=2)
+        self.add_student_button = tk.Button(self.add_student_window, text='Add', width=10, command=self.add_student)
+        self.add_student_button.grid(row=4, column=1, padx=2, pady=2)
+
+    def add_student(self):
+        s = self.add_student_entry.get().replace(', ', ',').split(',')
+        if len(s) != len(self.subject_headers):
+            mb.showerror("Error!", "Wrong input data")
+        else:
+            print(s)
 
     def press_del_student(self):
-        pass
+        answer = mb.askyesno(title="Removing", message="Do you want to remove selected student?")
+        if answer:
+            print('Student is removed')
 
 
 class Table(tk.Frame):
